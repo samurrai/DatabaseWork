d32adff555a4e94d7bb7fc195705efa378ab810c
кирилл Захаров <csgo2003@mail.ru>
2019-04-01 17:47:59 +0600

    commit

---
+++ ConnectedLevelApp.DataAccess/UsersTableService.cs
@@ -0,0 +1,97 @@
+﻿using ConnectedLevelApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace ConnectedLevelApp.DataAccess
+{
+    public class UsersTableService
+    {
+
+        private readonly string _connectionString = "";
+
+        public UsersTableService()
+        {
+            _connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;
+                                AttachDbFilename=C:\Users\ШадикьянР\source\repos\ConnectedLevelApp\ConnectedLevelApp.DataAccess\Database.mdf;
+                                Integrated Security=True";
+        }
+
+        public List<User> SelectUsers()
+        {
+            var data = new List<User>();
+
+            using (var connection = new SqlConnection(_connectionString))
+            using (var command = connection.CreateCommand())
+            {
+                try
+                {
+                    connection.Open();
+                    command.CommandText = "select * from Users";
+
+                    var sqlDataReader = command.ExecuteReader();
+
+                    while (sqlDataReader.Read())
+                    {
+                        int id = (int)sqlDataReader["Id"];
+                        string password = sqlDataReader["Login"].ToString();
+                        string login = sqlDataReader["Password"].ToString();
+
+                        data.Add(new User() {
+                            Id = id,
+                            Login = login,
+                            Password = password
+                        });
+                    }
+                }
+                catch (SqlException exception)
+                {
+                    // обработать
+                    throw;
+                }
+                catch (Exception exception)
+                {
+                    // обработать
+                    throw;
+                }
+            }
+            return data;
+        }
+        public void InsertUser(User user)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            using (var command = connection.CreateCommand())
+            {
+                try
+                {
+                    connection.Open();
+                    command.CommandText = $"insert into Users values('{user.Login}', '{user.Password}')";
+                    int affectedRows = command.ExecuteNonQuery();
+
+                    if (affectedRows < 1)
+                    {
+                        throw new Exception("Вставка не удалась");
+                    }
+                }
+                catch (SqlException exception)
+                {
+                    // обработать
+                    throw;
+                }
+                catch (Exception exception)
+                {
+                    // обработать
+                    throw;
+                }
+            }
+        }
+        public void DeleteUserById(int id)
+        {
+
+        }
+        public void UpdateUser(User user)
+        {
+
+        }
+    }
+}
---
+++ ConnectedLevelApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using ConnectedLevelApp.DataAccess;
+using ConnectedLevelApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectedLevelApp.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UsersTableService usersTableService = new UsersTableService();
+            var user = new User()
+            {
+                Login = "admin",
+                Password = "root"
+            };
+            usersTableService.InsertUser(user);
+
+            foreach (var item in usersTableService.SelectUsers())
+            {
+                System.Console.WriteLine($"{item.Login}, {item.Password}");
+            }
+            System.Console.ReadLine();
+        }
+    }
+}
